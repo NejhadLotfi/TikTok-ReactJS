6b0ebcc540fba67c4e0ed2807919aaa9c5df7bd2
Hossein <djalicrt@gmail.com>
2020-11-21 18:44:40 +0330

    First Part :: Video Component init.

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1RGXhpcAyrtZnTLBh3YxaiZauRCOLhkw",
+    authDomain: "tiktok-e6cd7.firebaseapp.com",
+    databaseURL: "https://tiktok-e6cd7.firebaseio.com",
+    projectId: "tiktok-e6cd7",
+    storageBucket: "tiktok-e6cd7.appspot.com",
+    messagingSenderId: "295177902128",
+    appId: "1:295177902128:web:b12b268175001fec36f5f5"
+  };
---
+++ src/Video.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Video.css';
+
+function Video() {
+    return (
+
+     <div className="video">
+        <h1>This is Video</h1>
+        { /* <VideoFooter /> */}
+        { /* <VideoSidebar /> */}
+        </div> 
+    );
+    
+}
+
+export default Video
